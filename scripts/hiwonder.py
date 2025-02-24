abf1616758905282647dd1baa33fb98c1cc5ce48
Bill Le <choangbill.le@gmail.com>
2025-02-24 17:28:38 -0500

    fix DH size

--- scripts/hiwonder.py
+++ scripts/hiwonder.py
@@ -26,7 +26,7 @@ class HiwonderRobot:
         # arm angles
         self.joint_values = [0, 0, 90, -30, 0, 0]  # degrees
         self.theta = [radians(i) for i in self.joint_values]
-        self.DH = [[0, 0, 0]]
+        self.DH = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]
 
         # arm lengths
         self.l1, self.l2, self.l3, self.l4, self.l5 = 0.155, 0.099, 0.095, 0.055, 0.105
