a0df2ea9d2bf2122045ca1046f9cd2962049497f
Bill Le <choangbill.le@gmail.com>
2025-03-26 15:45:09 -0400

    solve ignored merge-conflict

--- scripts/gamepad_control.py
+++ scripts/gamepad_control.py
@@ -17,15 +17,9 @@ class GamepadControl:
         """Initialize the gamepad and internal state variables."""
         self.initialize_gamepad()
         self.gamepad_cmds_prev = GamepadCmds()
-<<<<<<< HEAD
-        self.abs_x = self.abs_y = self.abs_z = self.abs_rz = (
-            128  # Default centered values
-        )
-=======
         # Default centered values
-        self.abs_x = 128  
-        self.abs_y = self.abs_z = -128 
->>>>>>> upstream/main
+        self.abs_x = 128
+        self.abs_y = self.abs_z = -128
 
         # Control flags
         self.MOBILE_BASE_FLAG = False
@@ -72,21 +66,57 @@ class GamepadControl:
                 # print(f'type: {event.code}, state: {event.state}')
 
         if self.MOBILE_BASE_FLAG:
-            gamepad_cmds.base_vx = self.map_value(self.abs_x, [-32767, 32767], [-0.5, 0.5])
-            gamepad_cmds.base_vy = self.map_value(self.abs_y, [32767, -32767], [-0.5, 0.5])
-            gamepad_cmds.base_w = self.map_value(self.abs_z,  [32767, -32767], [-0.5, 0.5])
+            gamepad_cmds.base_vx = self.map_value(
+                self.abs_x, [-32767, 32767], [-0.5, 0.5]
+            )
+            gamepad_cmds.base_vy = self.map_value(
+                self.abs_y, [32767, -32767], [-0.5, 0.5]
+            )
+            gamepad_cmds.base_w = self.map_value(
+                self.abs_z, [32767, -32767], [-0.5, 0.5]
+            )
 
         if self.ARM_FLAG:
-            gamepad_cmds.arm_vx = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
-            gamepad_cmds.arm_vy = self.map_value(self.abs_y, [32767, -32767], [-0.1, 0.1])
-            gamepad_cmds.arm_vz = self.map_value(self.abs_z, [32767, -32767], [-0.1, 0.1])
+            gamepad_cmds.arm_vx = self.map_value(
+                self.abs_x, [-32767, 32767], [-0.1, 0.1]
+            )
+            gamepad_cmds.arm_vy = self.map_value(
+                self.abs_y, [32767, -32767], [-0.1, 0.1]
+            )
+            gamepad_cmds.arm_vz = self.map_value(
+                self.abs_z, [32767, -32767], [-0.1, 0.1]
+            )
 
-        gamepad_cmds.arm_j1 = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_J1_FLAG else 0.0
-        gamepad_cmds.arm_j2 = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_J2_FLAG else 0.0
-        gamepad_cmds.arm_j3 = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_J3_FLAG else 0.0
-        gamepad_cmds.arm_j4 = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_J4_FLAG else 0.0
-        gamepad_cmds.arm_j5 = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_J5_FLAG else 0.0
-        gamepad_cmds.arm_ee = self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1]) if self.ARM_EE_FLAG else 0.0
+        gamepad_cmds.arm_j1 = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_J1_FLAG
+            else 0.0
+        )
+        gamepad_cmds.arm_j2 = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_J2_FLAG
+            else 0.0
+        )
+        gamepad_cmds.arm_j3 = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_J3_FLAG
+            else 0.0
+        )
+        gamepad_cmds.arm_j4 = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_J4_FLAG
+            else 0.0
+        )
+        gamepad_cmds.arm_j5 = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_J5_FLAG
+            else 0.0
+        )
+        gamepad_cmds.arm_ee = (
+            self.map_value(self.abs_x, [-32767, 32767], [-0.1, 0.1])
+            if self.ARM_EE_FLAG
+            else 0.0
+        )
         gamepad_cmds.arm_home = int(self.ARM_HOME)
 
         self.gamepad_cmds_prev = gamepad_cmds
@@ -95,18 +125,18 @@ class GamepadControl:
     def _handle_event(self, event):
         """Handles individual gamepad events and updates internal state."""
         code_map = {
-            'ABS_X': ('abs_x', event.state),
-            'ABS_Y': ('abs_y', event.state),
-            'ABS_RY': ('abs_z', event.state),
-            'BTN_TL': ('MOBILE_BASE_FLAG', bool(event.state)),
-            'BTN_TR': ('ARM_FLAG', bool(event.state)),
-            'BTN_WEST': ('ARM_J1_FLAG', bool(event.state)),
-            'BTN_EAST': ('ARM_J2_FLAG', bool(event.state)),
-            'BTN_SOUTH': ('ARM_J3_FLAG', bool(event.state)),
-            'BTN_NORTH': ('ARM_J4_FLAG', bool(event.state)),
-            'ABS_RZ': ('ARM_J5_FLAG', bool(event.state)),
-            'ABS_Z': ('ARM_EE_FLAG', bool(event.state)),
-            'BTN_SELECT': ('ARM_HOME', bool(event.state))
+            "ABS_X": ("abs_x", event.state),
+            "ABS_Y": ("abs_y", event.state),
+            "ABS_RY": ("abs_z", event.state),
+            "BTN_TL": ("MOBILE_BASE_FLAG", bool(event.state)),
+            "BTN_TR": ("ARM_FLAG", bool(event.state)),
+            "BTN_WEST": ("ARM_J1_FLAG", bool(event.state)),
+            "BTN_EAST": ("ARM_J2_FLAG", bool(event.state)),
+            "BTN_SOUTH": ("ARM_J3_FLAG", bool(event.state)),
+            "BTN_NORTH": ("ARM_J4_FLAG", bool(event.state)),
+            "ABS_RZ": ("ARM_J5_FLAG", bool(event.state)),
+            "ABS_Z": ("ARM_EE_FLAG", bool(event.state)),
+            "BTN_SELECT": ("ARM_HOME", bool(event.state)),
         }
 
         if event.code in code_map:
@@ -125,5 +155,7 @@ class GamepadControl:
             float: Mapped value.
         """
         # val = (x - joint_min) * (out_max - out_min) / (joint_max - joint_min) + out_min
-        val = (x - in_range[0]) * (out_range[1] - out_range[0]) / (in_range[1] - in_range[0]) + out_range[0]
+        val = (x - in_range[0]) * (out_range[1] - out_range[0]) / (
+            in_range[1] - in_range[0]
+        ) + out_range[0]
         return val if abs(val) > 0.005 else 0.0
