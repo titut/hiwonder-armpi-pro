8152de018830a75ceb14afad857e1e61e3d679c2
Bill Le <choangbill.le@gmail.com>
2025-03-31 16:17:18 -0400

    change movement duration

--- scripts/hiwonder.py
+++ scripts/hiwonder.py
@@ -407,22 +407,22 @@ class HiwonderRobot:
         print(f"Moving to square position...")
         self.set_joint_values(
             self.calc_analytical_inverse_kinematics(0.29, -0.0762, 0.331, -30),
-            duration=800,
+            duration=2000,
         )
         time.sleep(2.0)
         self.set_joint_values(
             self.calc_analytical_inverse_kinematics(0.29, 0.0762, 0.331, -30),
-            duration=800,
+            duration=2000,
         )
         time.sleep(2.0)
         self.set_joint_values(
             self.calc_analytical_inverse_kinematics(0.29, 0.0762, 0.179, 30),
-            duration=800,
+            duration=2000,
         )
         time.sleep(2.0)
         self.set_joint_values(
             self.calc_analytical_inverse_kinematics(0.29, -0.0762, 0.179, 30),
-            duration=800,
+            duration=2000,
         )
         time.sleep(1.0)
         print(f"-------------------Moved to Square!------------------- \n")
